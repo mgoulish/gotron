fe562b4f030060f4da1cef68fb85de69e88f04a4
mgoulish <mgoulish@redhat.com>
2020-12-16 04:00:26 -0500

    This version successfully communicates between Brontonomicon and Colossus-Guardian when the firewalls are turned off, and one instance is run on each machine with the respective run-scripts.

--- gotron.go
+++ gotron.go
@@ -13,15 +13,14 @@ import (
 var fp = fmt.Fprintf
 
 
-var total_bytes int
 
 
-
-func cnx_handler ( name       string,
+func cnx_handler ( my_name    string,
                    cnx_number int, 
                    cnx        net.Conn ) {
 
   buffer := make ( []byte, 512 )
+  total_bytes := 0
 
   for {
     var   n   int
@@ -31,20 +30,17 @@ func cnx_handler ( name       string,
 
     if err != nil {
       if err != io.EOF {
-        fp ( os.Stderr, "%s error : connection read : |%s|\n", name, err.Error() )
+        fp ( os.Stderr, "%s error : connection read : |%s|\n", my_name, err.Error() )
       }
       break
     }
 
     if n > 0 {
       message := string(buffer[0:n])
-      fp ( os.Stdout, "%s : received from cnx %d : |%s|\n", name, cnx_number, message )
+      fp ( os.Stdout, "%s : received from cnx %d : |%s|\n", my_name, cnx_number, message )
       total_bytes += len ( message )
-      fp ( os.Stdout, "%s has received %d total bytes.\n", name, total_bytes )
+      fp ( os.Stdout, "%s has received %d total bytes.\n", my_name, total_bytes )
     }
-    
-    //experiment 
-    cnx.Write ( buffer )
   }
 }
 
@@ -54,12 +50,12 @@ func cnx_handler ( name       string,
 
 // This can accept multiple connections, and will 
 // launch a goroutine for each.
-func listen ( name string, port string ) {
+func listen ( my_name string, my_addr, port string ) {
   cnx_count := 0
 
-  tcp_listener, err := net.Listen ( "tcp", ":" + port )
+  tcp_listener, err := net.Listen ( "tcp", my_addr + ":" + port )
   if err != nil {
-    fp ( os.Stdout, "name error : net.Listen error |%s|\n", name, err.Error() )
+    fp ( os.Stdout, "%s error : net.Listen error |%s|\n", my_name, err.Error() )
     os.Exit ( 1 )
   }
 
@@ -67,13 +63,13 @@ func listen ( name string, port string ) {
     cnx, err := tcp_listener.Accept ( )
 
     if err != nil {
-      fp ( os.Stdout, "name error : |%s|\n", name, err.Error() )
+      fp ( os.Stdout, "%s error : |%s|\n", my_name, err.Error() )
       os.Exit(1)
     }
     fp ( os.Stdout, "accept returned.\n" )
 
     cnx_count ++
-    go cnx_handler ( name, cnx_count, cnx )
+    go cnx_handler ( my_name, cnx_count, cnx )
   }
 }
 
@@ -81,46 +77,53 @@ func listen ( name string, port string ) {
 
 
 
-func dialer ( name string, host, port string, timeout_seconds int ) ( cnx net.Conn, err error ) {
+func dialer ( my_name string, other_addr, other_port string, timeout_seconds int ) ( cnx net.Conn, err error ) {
 
   for t := 0; t < timeout_seconds; t ++ {
-    cnx, err := net.Dial ( "tcp", host + ":" + port )
+    cnx, err := net.Dial ( "tcp", other_addr + ":" + other_port )
     if err == nil {
       return cnx, nil
     }
 
-    time.Sleep ( time.Second )
+    fp ( os.Stdout, "dialer: |%s|\n", err.Error() )
+    time.Sleep ( 2 * time.Second )
   }
 
-  return nil, fmt.Errorf ( "%s : timed out", name )
+  return nil, fmt.Errorf ( "%s : timed out", my_name )
 }
 
 
 
 
 
-func make_connection ( name string, n_messages int, host, port string ) {
-  cnx, err := dialer ( name, host, port, 10 )
+func make_connection ( my_name string, n_messages int, other_addr, other_port string ) {
+
+  cnx, err := dialer ( my_name, other_addr, other_port, 20 )
 
   if err != nil {
-    fp ( os.Stdout, "%s error: |%s|\n", name, err.Error() )
+    fp ( os.Stdout, "%s error: |%s|\n", my_name, err.Error() )
     os.Exit ( 1 )
   }
 
   if cnx == nil {
-    fp ( os.Stdout, "%s error : nil connection.\n", name )
+    fp ( os.Stdout, "%s error : nil connection.\n", my_name )
     os.Exit ( 2 )
   }
 
   defer cnx.Close ( )
 
   message := "0123456789"
+  total_bytes_sent := 0
 
   for i := 0; i < n_messages; i ++ {
     fp ( cnx, message )
-    fp ( os.Stdout, "%s sent |%s|\n", name, message )
-    time.Sleep ( 5 * time.Second )
+    total_bytes_sent += len(message)
+    fp ( os.Stdout, "%s sent |%s|  total bytes sent: %d\n", 
+         my_name, message, total_bytes_sent )
+    time.Sleep ( 3 * time.Second )
   }
+
+  fp ( os.Stdout, "%s is done sending.\n", my_name )
 }
 
 
@@ -128,16 +131,16 @@ func make_connection ( name string, n_messages int, host, port string ) {
 
 
 func main ( ) {
-  name          := os.Args[1]
-  host          := os.Args[2]
-  incoming_port := os.Args[3]
-  outgoing_port := os.Args[4]
+  my_name      := os.Args[1]
+  my_addr      := os.Args[2]
+  my_port      := os.Args[3]
+  other_addr   := os.Args[4]
+  other_port   := os.Args[5]
 
-  go listen ( name, incoming_port )
+  go listen ( my_name, my_addr, my_port )
 
   n_messages := 10
-  go make_connection ( name, n_messages, host, outgoing_port )
-
+  go make_connection ( my_name, n_messages, other_addr, other_port )
 
   for {
     time.Sleep ( 100 * time.Second )
